85695fbdcb87987ce2193eb895d855445dbffaf2
Zach Timberlake <zachindeed@gmail.com>
2025-08-10 16:10:44 -0700

    Exam two Data structures

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,7 +1,26 @@
 /**
  * Must use seperate chaining strategy
  */
+import com.sun.jdi.ObjectReference;
+
+import java.util.LinkedList;
 public class HashTable
 {
+    private class Record
+    {
+        private Object key;
+        private Object value;
 
+        public Record()
+        {
+            this.key = null;
+            this.value = null;
+        }
+        public Record(Object inKey, Object inData)
+        {
+            this.key = inKey;
+            this.value = inData;
+        }
+
+    }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -1,4 +1,51 @@
+import java.util.EmptyStackException;
+
 public class Stack
 {
+    private class Node
+    {
+        private Object data;
+        private Node next;
 
+        public Node(Object element, Node node)
+        {
+            this.data = element;
+            this.next = node;
+        }
+    }//end node class
+
+    protected Node top;
+    protected int size;
+
+    public Stack()
+    {
+        this.top = null;
+        this.size = 0;
+    }
+
+    public int size(){return size;}
+
+    public boolean isEmpty()
+    {
+        if (top == null)
+            return true;
+        return false;
+    }
+
+    public void push(Object element)
+    {
+        Node nn = new Node(element, top);
+        this.top = nn;
+        this.size++;
+    }
+
+    public Object pop() throws EmptyStackException
+    {
+        if (isEmpty())
+            throw new EmptyStackException();
+        Object temp = top.data;
+        this.top = top.next;
+        this.size--;
+        return temp;
+    }
 }
